b29b7047a13d689a531a2ea74b493d185c2cfe7c
lohjo <lohjohn02@GMAIL.COM>
2025-08-27 13:14:56 +0800

    Create ex3.ino

---
+++ Autonomous/Autonomos_code/ex3/ex3.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  my_robot.MoveForward(50);
+  delay(3000);
+  my_robot.MoveForward(50);
+  delay(3000);
+
+}
